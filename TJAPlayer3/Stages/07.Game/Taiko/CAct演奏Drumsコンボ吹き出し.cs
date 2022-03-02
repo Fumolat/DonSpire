d2f2b475d7dd26df6ed992457e2a4765edacebe5
Jamix <52881627+JamnCN@users.noreply.github.com>
2022-03-02 13:20:19 +0900

    Update CAct演奏Drumsコンボ吹き出し.cs

--- TJAPlayer3/Stages/07.Game/Taiko/CAct演奏Drumsコンボ吹き出し.cs
+++ TJAPlayer3/Stages/07.Game/Taiko/CAct演奏Drumsコンボ吹き出し.cs
@@ -31,7 +31,7 @@ namespace TJAPlayer3
         public virtual void Start( int nCombo, int player )
 		{
             this.NowDrawBalloon = 0;
-            this.ct進行[ player ] = new CCounter( 1, 42, 70, TJAPlayer3.Timer );
+            this.ct進行[ player ] = new CCounter( 1, 42, 42, TJAPlayer3.Timer );
             this.nCombo_渡[player] = nCombo;
 		}
 
